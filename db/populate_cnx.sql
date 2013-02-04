c0f1c31fcc4c91f387b87aba9c4ced0b3d2872e8
Michael Mulich <michael.mulich@gmail.com>
2013-02-04 16:57:53 -0500

    For whatever reason the prepopulation of various rows throws off sequencial ids (at least on my system). Therefore, I'll remove them to keep things good. Besides, the example values aren't critically important.

--- db/populate_cnx.sql
+++ db/populate_cnx.sql
@@ -12,17 +12,6 @@ COPY arch (id, name) FROM stdin;
 
 
 --
--- Data for Name: buildclients; Type: TABLE DATA; Schema: public; Owner: reedstrm
---
-
-COPY buildclients (id, name) FROM stdin;
-1	arm01
-2	arm02
-3	buildbox
-\.
-
-
---
 -- Data for Name: distribution; Type: TABLE DATA; Schema: public; Owner: reedstrm
 --
 
@@ -43,49 +32,6 @@ COPY format (id, name) FROM stdin;
 4	mobi
 5	completezip
 \.
-
-
---
--- Data for Name: job; Type: TABLE DATA; Schema: public; Owner: reedstrm
---
-
-COPY job (id, packageinstance_id, buildclient_id) FROM stdin;
-1	6	\N
-\.
-
-
---
--- Data for Name: jobstatus; Type: TABLE DATA; Schema: public; Owner: reedstrm
---
-
-COPY jobstatus (id, job_id, status_id, "time") FROM stdin;
-1	1	1	2012-11-29 16:03:28.78304
-\.
-
-
---
--- Data for Name: package; Type: TABLE DATA; Schema: public; Owner: reedstrm
---
-
-COPY package (id, version, name) FROM stdin;
-3	23	collection2345
-4	1.2	m9003
-\.
-
-
---
--- Data for Name: packageinstance; Type: TABLE DATA; Schema: public; Owner: reedstrm
---
-
-COPY packageinstance (id, package_id, arch_id, suite_id, dist_id, format_id, master) FROM stdin;
-6	3	9	3	3	2	t
-7	4	5	3	2	2	f
-8	4	12	3	3	5	f
-9	3	12	4	3	5	f
-10	4	12	4	3	4	f
-11	3	12	3	3	5	f
-\.
-
 
 --
 -- Data for Name: status; Type: TABLE DATA; Schema: public; Owner: reedstrm
@@ -121,5 +67,3 @@ COPY suitearches (id, suite_id, arch_id) FROM stdin;
 7	4	9
 8	4	5
 \.
-
-
